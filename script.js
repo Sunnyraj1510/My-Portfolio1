2ba4dc8e8588b2b83e4f1bd37e5c31ed457c4566
Sunnyraj1510 <sunnyraj1510@gmail.com>
2023-09-21 18:38:57 +0530

    fixed some issues

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*toggle icon navbar*/
-let menuIcon = documnet.querySelector('#menu-icon');
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () =>{
@@ -19,7 +19,7 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top<offset+height){
+        if(top >= offset && top < offset + height){
             navLinks.forEach(links=>{
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' +id+ ']').classList.add('active');
@@ -29,7 +29,7 @@ window.onscroll = () => {
 
     /*sticky navbar*/
     let header = document.querySelector('header');
-    header.classList.toggle('sticky',window.screenY>100)
+    header.classList.toggle('sticky',window.scrollY>100);
 
     /*remove toggle icon and navbar when click navbar link(scroll)*/
     menuIcon.classList.remove('bx-x');
@@ -45,6 +45,6 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-conatiner, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .project-conatiner, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
